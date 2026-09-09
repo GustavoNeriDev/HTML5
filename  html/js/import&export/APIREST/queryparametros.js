97aa0cd219671231cef4608bd646c5314746443c
Gustavo Neri <nerigustavo828@gmail.com>
2026-09-08 21:52:08 -0300

    query params

---
+++  html/js/import&export/APIREST/queryparametros.js
@@ -0,0 +1,30 @@
+const API_URL = "https://fakestoreapi.com/products";
+
+export async function buscarPorCategoria(categoria) {
+  //cria e manipula os parametros URL
+  const params = new URLSearchParams({
+    category: categoria,
+  });
+  const response = await fetch(`${API_URL}?${params}`);
+  if (!response.ok) {
+    throw new Error("erro");
+  }
+
+  return response.json();
+}
+
+//// atividade
+
+const API_URL = "https://fakestoreapi.com/products";
+
+export async function buscarPorCategoria(categoria) {
+  const params = new URLSearchParams({
+    category: categoria,
+  });
+  const response = await fetch(`${API_URL}?${params}`);
+  if (!response.ok) {
+    throw new Error("erro");
+  }
+
+  return response.json();
+}
